43bce046c1fff228e25d3b1d3807e1d216f44b8a
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 16:34:53 +0300

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+from app.core.config import settings
+
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+    future=True,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+Base = declarative_base()

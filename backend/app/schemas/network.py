d95bcb876f43d6656ffbd664c41de71994ec0232
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 16:25:20 +0300

    Update network.py

--- backend/app/schemas/network.py
+++ backend/app/schemas/network.py
@@ -1,1 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional
+import uuid
+from datetime import datetime
 
+class NetworkInterfaceBase(BaseModel):
+    interface: str
+    ip: Optional[str] = None
+    mac: Optional[str] = None
+    type: str
+    ssid: Optional[str] = None
+    status: str
+    signal_strength: Optional[int] = None
+    speed_mbps: Optional[int] = None
+
+class NetworkInterfaceCreate(NetworkInterfaceBase):
+    pass
+
+class NetworkInterfaceUpdate(BaseModel):
+    ip: Optional[str]
+    mac: Optional[str]
+    status: Optional[str]
+    signal_strength: Optional[int]
+    speed_mbps: Optional[int]
+
+class NetworkInterfaceOut(NetworkInterfaceBase):
+    id: uuid.UUID
+    created: datetime
+
+    class Config:
+        orm_mode = True

a67cd7d93798781eab27a725bc0bb437ff843a54
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 18:04:32 +0300

    Update users.ts

--- frontend/src/api/users.ts
+++ frontend/src/api/users.ts
@@ -1,1 +1,38 @@
+import axios from "axios";
+import { API_BASE } from "../utils/config";
+import { User } from "../types/user";
 
+// List users
+export async function listUsers(): Promise<User[]> {
+  const token = localStorage.getItem("token");
+  const res = await axios.get(`${API_BASE}/users`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.users;
+}
+
+// Add new user
+export async function addUser(user: Partial<User>): Promise<User> {
+  const token = localStorage.getItem("token");
+  const res = await axios.post(`${API_BASE}/users`, user, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.user;
+}
+
+// Edit user
+export async function editUser(id: string, data: Partial<User>): Promise<User> {
+  const token = localStorage.getItem("token");
+  const res = await axios.put(`${API_BASE}/users/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.user;
+}
+
+// Delete user
+export async function deleteUser(id: string): Promise<void> {
+  const token = localStorage.getItem("token");
+  await axios.delete(`${API_BASE}/users/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+}

cf5cba687e3575c9f3a5f304b1a5d7b4d200e40c
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 16:22:25 +0300

    Update firewall.py

--- backend/app/schemas/firewall.py
+++ backend/app/schemas/firewall.py
@@ -1,1 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional
+import uuid
 
+class FirewallRuleBase(BaseModel):
+    name: str
+    direction: str
+    protocol: str
+    port: str
+    action: str
+
+class FirewallRuleCreate(FirewallRuleBase):
+    pass
+
+class FirewallRuleUpdate(BaseModel):
+    name: Optional[str]
+    direction: Optional[str]
+    protocol: Optional[str]
+    port: Optional[str]
+    action: Optional[str]
+
+class FirewallRuleOut(FirewallRuleBase):
+    id: uuid.UUID
+
+    class Config:
+        orm_mode = True

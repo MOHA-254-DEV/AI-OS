8e531a17717b30877b9afc42564c831d3a91be6f
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 17:39:37 +0300

    Update TabPanel.tsx

--- frontend/src/components/TabPanel.tsx
+++ frontend/src/components/TabPanel.tsx
@@ -1,1 +1,34 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SystemTab from "./SystemTab";
+import FilesTab from "./FilesTab";
+import SettingsTab from "./SettingsTab";
+import SearchTab from "./SearchTab";
+import UserTab from "./UserTab";
+import NetworkTab from "./NetworkTab";
+import TerminalTab from "./TerminalTab";
 
+function TabPanel({
+  selectedTab,
+  darkMode,
+  setDarkMode,
+}: {
+  selectedTab: number;
+  darkMode: boolean;
+  setDarkMode: (n: boolean) => void;
+}) {
+  // Render based on router path (for deep-linking/multi-tab support)
+  return (
+    <Routes>
+      <Route path="/" element={<SystemTab />} />
+      <Route path="/files" element={<FilesTab />} />
+      <Route path="/settings" element={<SettingsTab darkMode={darkMode} setDarkMode={setDarkMode} />} />
+      <Route path="/search" element={<SearchTab />} />
+      <Route path="/users" element={<UserTab />} />
+      <Route path="/network" element={<NetworkTab />} />
+      <Route path="/terminal" element={<TerminalTab />} />
+    </Routes>
+  );
+}
+
+export default TabPanel;

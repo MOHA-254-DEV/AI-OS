3245198be3ce697eeab730d3e8643877c2cdc28b
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-26 16:28:49 +0300

    Update file.py

--- backend/app/schemas/file.py
+++ backend/app/schemas/file.py
@@ -1,1 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+import uuid
+from datetime import datetime
 
+class FileBase(BaseModel):
+    name: str
+    path: str
+    type: str
+    size: Optional[int] = 0
+    modified: Optional[datetime] = None
+    preview_url: Optional[str] = None
+
+class FileCreate(FileBase):
+    pass
+
+class FileUpdate(BaseModel):
+    name: Optional[str]
+    path: Optional[str]
+    type: Optional[str]
+    size: Optional[int]
+    preview_url: Optional[str]
+
+class FileOut(FileBase):
+    id: uuid.UUID
+    owner_id: Optional[uuid.UUID]
+
+    class Config:
+        orm_mode = True

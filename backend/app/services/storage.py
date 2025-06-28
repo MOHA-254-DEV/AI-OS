3691b255758398920217822c75fc9484ab1020a6
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-28 20:47:51 +0300

    Update storage.py

--- backend/app/services/storage.py
+++ backend/app/services/storage.py
@@ -1,1 +1,13 @@
+import os
 
+def save_file(file_obj, save_path: str):
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    with open(save_path, "wb") as f:
+        f.write(file_obj.read())
+    return save_path
+
+def delete_file(save_path: str):
+    if os.path.exists(save_path):
+        os.remove(save_path)
+        return True
+    return False

55750524f8b27d2cb7b4f64a63c5d3be1db78b30
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-28 13:39:21 +0300

    Update organization.py

--- backend/app/schemas/organization.py
+++ backend/app/schemas/organization.py
@@ -1,1 +1,23 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
 
+class OrganizationBase(BaseModel):
+    name: str
+    display_name: Optional[str] = None
+    description: Optional[str] = None
+
+class OrganizationCreate(OrganizationBase):
+    pass
+
+class OrganizationUpdate(BaseModel):
+    display_name: Optional[str] = None
+    description: Optional[str] = None
+
+class OrganizationOut(OrganizationBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True

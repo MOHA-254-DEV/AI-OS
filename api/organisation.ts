ce4742b9f1d1b6d46fc1a68041e0c95122efda35
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-29 20:32:06 +0300

    Delete api directory

--- api/organisation.ts
+++
@@ -1,7 +0,0 @@
-import api from "../utils/apiClient";
-import { Organization } from "../types/organization";
-
-export async function getMyOrganizations(): Promise<Organization[]> {
-  const res = await api.get("/organizations/my");
-  return res.data;
-}

a7c9e709b3b5435456c876b263e371c029df7cc7
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-29 19:39:08 +0300

    Update notification.ts

--- api/notification.ts
+++ api/notification.ts
@@ -1,1 +1,7 @@
+import api from "../utils/apiClient";
+import { Notification } from "../types/notification";
 
+export async function getMyNotifications(): Promise<Notification[]> {
+  const res = await api.get("/notifications/");
+  return res.data;
+}

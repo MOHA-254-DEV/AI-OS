efc228dedbd67755737b8df34cc9eec0c77df051
MOHA-254-DEV <smartworksinc254@gmail.com>
2025-06-29 12:12:27 +0300

    Update Organizations.tsx

--- frontend/src/components/Dashboard/Organizations.tsx
+++ frontend/src/components/Dashboard/Organizations.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+import { useOrganization } from "../../context/OrganizationContext";
+import { formatDateTime } from "../../utils/helpers";
 
+const Organizations: React.FC = () => {
+  const { organizations } = useOrganization();
+  return (
+    <div>
+      <h2>Organizations</h2>
+      <ul>
+        {organizations.map((org) => (
+          <li key={org.id}>
+            <b>{org.name}</b> (Created: {formatDateTime(org.createdAt)})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Organizations;
